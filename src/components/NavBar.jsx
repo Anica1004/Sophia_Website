0022418b7fbeddd4e8404d67216078effae95295
Anica1004 <anicamok@gmail.com>
2025-10-30 02:11:02 -0700

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import './NavBar.css'
+import { FaLinkedin } from 'react-icons/fa';
+
+export default function NavBar(){
+
+    return (
+       <nav>
+        <h1>Sophia (Sumin) Mok</h1>
+        <ul className = "nav-links">
+            <li><a className = "link" href = "#TBD">Demo Reel</a></li>
+            <li><a className = "link" href = "#TBD">Portfolio</a></li>
+            <li><a className = "link" href = "#TBD">Resume</a></li>
+            <li><a className = "link" href = "#TBD">Articles</a></li>
+            <li>
+        <a
+            href="https://www.linkedin.com/in/sophiasuminmok"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="linkedin-icon"
+        >
+            in
+        </a>
+        </li>
+        </ul>
+       </nav>
+    ); 
+
+}
