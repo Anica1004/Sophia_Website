c17fdde375c43542a7c0b2c0fef5fc0699583b24
Anica1004 <anicamok@gmail.com>
2025-11-01 00:20:45 -0700

    Completed articles tab

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,16 +6,18 @@ export default function Article({title, content, video_link="", link}){
         <div>
             <a className="header" href={link}>{title}</a>
             <p className="body" dangerouslySetInnerHTML={{ __html: content }} />
-            {video_link && <iframe
-            title="vimeo-player"
+            {video_link && (
+        <div className="video">
+          <iframe
+            title="video"
             src={video_link}
-            width="560"
-            height="315"
-            frameborder="0"
-            referrerpolicy="strict-origin-when-cross-origin"
+            frameBorder="0"
+            referrerPolicy="strict-origin-when-cross-origin"
             allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-            allowfullscreen
-        ></iframe>}
+            allowFullScreen
+          />
+        </div>
+      )}
         </div>
     );
 }
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,16 +1,19 @@
 import Article from '../components/Article'
+import './Articles.css'
 
 
 export default function Articles(){
 
     return (
-        <div>
+        <div className='container'>
+            <div className='articles-container'>
             <Article title="Announcing the 2023 Graduation Award Winners" content='
 An Emily Carr University of Art + Design article mentions that I received the Sophie Burnett Memorial Award and an honourable mention for the The President’s Media Awards for Excellence (Best in 3D Computer Animation) for their project, <em>Documents of Life</em>.'
 link="https://www.ecuad.ca/news/2023/the-show-student-awards-2023"/>
             <Article title="Convocation: Congratulations to the Class of 2023!" content="
 An Emily Carr University of Art + Design article mentions that I was the speaker for the graduation ceremony for class of 2023. The video records my speech from 1:29:50."
 video_link="https://www.youtube.com/embed/tmmq6tkQMqk?si=8jFZWpPDexd2yFll" link="https://www.ecuad.ca/updates/congratulations-to-the-class-of-2023"/>
+        </div>
         </div>
         
     );
