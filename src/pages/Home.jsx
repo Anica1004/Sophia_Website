0230e0629554a4b8ca633ccb18a59938a094fffd
Anica1004 <anicamok@gmail.com>
2026-08-04 00:20:20 -0700

    fixed reel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home(){
         </div>
         <div className = "demo-reels">
           <DemoReel link="https://player.vimeo.com/video/806201784?h=ee34fd0feb" title="PERSONAL TEXTURE & LOOKDEV REEL"/>
-          <DemoReel link="https://www.youtube.com/embed/WehkIlTBwiM?si=hMU9lwff-Vq9XuXn" title="PROFESSIONAL TEXTURE & LOOKDEV REEL"/>
+          <DemoReel link="https://www.youtube.com/embed/HIPZnoRLsOw?si=Ip73dEFAeENCOYfu" title="PROFESSIONAL TEXTURE REEL"/>
           <DemoReel link="https://player.vimeo.com/video/829171109?h=f7c951bad6" title="PERSONAL COMPOSITING REEL"/>
           <DemoReel link="https://player.vimeo.com/video/1011143104?h=3894bcfb5c" title="PROFESSIONAL LIGHTING AND COMPOSITING REEL"/>
           <DemoReel link="https://www.youtube.com/embed/3ib-F2JUpUo?si=Er79SOhpxjhx17DV" title="STUDENT SHORT FILM"/>
